e448552d3a05624be34228dca02179bf1e6223f1
marenoztro <marlon.marotto@gmail.com>
2022-08-08 23:29:38 +0000

    tomado de los apuntes de rosinni

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// ES5 => var
+
+// ES6 => let y const
+
+
+// let x = 31; //creando un nuevo espacio de memeoria llamdo x =31
+
+// if (true) {//true
+//     // x = 71; // reasignacion de valor
+//     let x = 71; // creando un nuevo espacio de memeoria llamdo x =71
+//     console.log("EStoy dentro del if: " + x); // output: 71
+
+// }else{//false
+
+// }
+
+// let frutas = [];
+
+//   !0
+// if (!frutas.length) { //true
+//     console.log("EL ARRAY ESTA VACIO");
+// }
+
+// if (!frutas.length === 0) { //true
+//     console.log("EL ARRAY ESTA VACIO");
+// }
+
+// if (!frutas.length != 0) { //true
+//     console.log("EL ARRAY ESTA VACIO");
+// }
+
+// //"" => false
+// let texto = prompt("Ingrese algo")
+// if (texto === "") { //true
+//     console.log("esta vacio");
+// }
+
+
+
+// console.log("EStoy fuera del if: " + x); //output: 71
+
+
+
+
+// Funciones: funciones de declaracion y funciones de expresion
+// saludar()
+
+function saludar() { // declaracion de una funcion de declaración
+    console.log("Hola a todos");
+}
+
+// saludar() //llamamos a la función
+// cantar()
+// datos de tipo objeto: funciones, arrays y objetos => const
+
+// const cantar = function() { //funcion de expresion
+//     saludar()
+//     console.log("soy una funcion sin nombre");
+// }
+
+
+// const bailar = ()=> { //arrows functions
+//     saludar()
+//     console.log("soy una funcion sin nombre");
+// }
+
+// bailar()
